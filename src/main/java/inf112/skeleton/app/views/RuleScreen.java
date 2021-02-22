6e71d6ddc5977f6b92b256e549b0f4a25fdb6614
Lars Holter <larsholter@hotmail.com>
2021-02-22 14:23:29 +0100

    Optimalisert imports

--- src/main/java/inf112/skeleton/app/views/RuleScreen.java
+++ src/main/java/inf112/skeleton/app/views/RuleScreen.java
@@ -1,18 +1,15 @@
 package inf112.skeleton.app.views;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import inf112.skeleton.app.RoboRallyGame;
 
 public class RuleScreen extends AbstractScreen {
