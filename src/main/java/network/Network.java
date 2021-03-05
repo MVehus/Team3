1a84428856c93e90f0a0ef3c74f16997eb0d5a0f
Mathias Vehus <mathias.vehus@student.no>
2021-03-05 13:15:45 +0100

    Enderinger i Network.java

--- src/main/java/network/Network.java
+++ src/main/java/network/Network.java
@@ -7,20 +7,25 @@ import java.util.ArrayList;
 public class Network {
 
     private static Server server;
+    private static Client client;
 
     public static void startServer(int port){
         server =  new Server(port);
     }
 
     public static Client makeNewClient(String IpAddress, int port){
-        return new Client(IpAddress, port);
+        client = new Client(IpAddress, port)
+    }
+
+    public static Client getClient() {
+        return client;
     }
 
     public static Server getServer(){
         return server;
     }
 
-    public static ArrayList<Connection> getClients(){
+    public static ArrayList<Connection> getAllClientsOnServer(){
         return server.getClients();
     }
 }
