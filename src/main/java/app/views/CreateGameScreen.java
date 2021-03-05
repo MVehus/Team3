5c0360f691c41d4d98e6487af81e903dfaac85d0
Lars Holter <larsholter@hotmail.com>
2021-03-05 13:35:12 +0100

    Støtte for tilkobling til server med GUI

--- src/main/java/app/views/CreateGameScreen.java
+++ src/main/java/app/views/CreateGameScreen.java
@@ -1,13 +1,12 @@
 package app.views;
 
+import app.ScreenOrchestrator;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
-import app.ScreenOrchestrator;
+import network.Network;
+import network.Server;
 
 public class CreateGameScreen extends AbstractScreen{
     public CreateGameScreen(ScreenOrchestrator screenOrchestrator) {
@@ -17,24 +16,47 @@ public class CreateGameScreen extends AbstractScreen{
     @Override
     public void show() {
         Skin skin = new Skin(Gdx.files.internal("src/assets/skin/plain-james/plain-james-ui.json"));
-        Table table = new Table();
-        table.setFillParent(true);
-        table.setDebug(false);
+        Table createServerTable = new Table();
+        createServerTable.setFillParent(true);
+        createServerTable.setDebug(false);
+
+        Label connectPortLabel = new Label("Select a port to create a server", skin);
+        Label portLabel = new Label("Port:", skin);
+        TextField portInput = new TextField("", skin);
+        TextButton createServerButton = new TextButton("Create Server", skin);
+        TextButton mainMenu = new TextButton("Main menu", skin);
+
+        createServerTable.add(connectPortLabel).colspan(3);
+        createServerTable.row().padTop(10);
+        createServerTable.add(portLabel);
+        createServerTable.add(portInput).width(85).padLeft(10);
+        createServerTable.add(createServerButton).padLeft(10);
+        createServerTable.row().padTop(10);
+        createServerTable.add(mainMenu).colspan(3);
+
+        stage.addActor(createServerTable);
+
+        Table createGameTable = new Table();
+        createGameTable.setFillParent(true);
+        createGameTable.setDebug(false);
 
         Label connectedLabel = new Label("Connected Players:", skin);
         TextButton createGameButton = new TextButton("Create Game", skin);
-        TextButton mainMenu = new TextButton("Main menu", skin);
-        Label localPlayer = new Label("blahblahipipip", skin); //TODO create connected player list
 
-        table.add(connectedLabel);
-        table.row().padTop(10);
-        table.add(localPlayer);
-        table.row().padTop(10);
-        table.add(createGameButton);
-        table.row().padTop(10);
-        table.add(mainMenu);
+        createGameTable.add(connectedLabel);
+        createGameTable.row().padTop(10);
+        createGameTable.add(createGameButton);
+        createGameTable.row().padTop(10);
 
-        stage.addActor(table);
+        createServerButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent changeEvent, Actor actor) {
+                Network.startServer(Integer.parseInt(portInput.getText()));
+                createServerTable.remove();
+                createGameTable.add(mainMenu);
+                stage.addActor(createGameTable);
+            }
+        });
 
         createGameButton.addListener(new ChangeListener() {
             @Override
--- src/main/java/app/views/JoinGameScreen.java
+++ src/main/java/app/views/JoinGameScreen.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import app.ScreenOrchestrator;
+import network.Client;
+import network.Network;
+
+import java.util.concurrent.ExecutionException;
 
 public class JoinGameScreen extends AbstractScreen{
     public JoinGameScreen(ScreenOrchestrator screenOrchestrator) {
@@ -14,29 +18,54 @@ public class JoinGameScreen extends AbstractScreen{
     @Override
     public void show() {
         Skin skin = new Skin(Gdx.files.internal("src/assets/skin/plain-james/plain-james-ui.json"));
-        Table table = new Table();
-        table.setFillParent(true);
-        table.setDebug(false);
+        Table joinGameTable = new Table();
+        joinGameTable.setFillParent(true);
+        joinGameTable.setDebug(false);
 
         TextButton mainMenu = new TextButton("Main menu", skin);
-        Label connectLabel = new Label("Connect to a host with an ip-address: ", skin);
-        Label ipLabel = new Label("ip:", skin);
-        Label portLabel = new Label("port:", skin);
+        Label connectLabel = new Label("Connect to a host with an IP-address: ", skin);
+        Label ipLabel = new Label("IP:", skin);
+        Label portLabel = new Label("Port:", skin);
         TextField ipInput = new TextField("", skin);
         TextField portInput = new TextField("", skin);
         TextButton connectButton = new TextButton("Connect", skin);
 
-        table.add(connectLabel).colspan(5);
-        table.row().padTop(10);
-        table.add(ipLabel);
-        table.add(ipInput).width(180).padLeft(10);
-        table.add(portLabel).padLeft(10);
-        table.add(portInput).width(85).padLeft(10);
-        table.add(connectButton).padLeft(10);
-        table.row().padTop(10);
-        table.add(mainMenu).colspan(5);
+        joinGameTable.add(connectLabel).colspan(5);
+        joinGameTable.row().padTop(10);
+        joinGameTable.add(ipLabel);
+        joinGameTable.add(ipInput).width(180).padLeft(10);
+        joinGameTable.add(portLabel).padLeft(10);
+        joinGameTable.add(portInput).width(85).padLeft(10);
+        joinGameTable.add(connectButton).padLeft(10);
+        joinGameTable.row().padTop(10);
+        joinGameTable.add(mainMenu).colspan(5);
 
-        stage.addActor(table);
+        stage.addActor(joinGameTable);
+
+        Table connectedTable = new Table();
+        connectedTable.setFillParent(true);
+        connectedTable.setDebug(false);
+
+        Label connectedLabel = new Label("Connected - waiting for host to start the game", skin);
+        Label connectionFailedLabel = new Label("Something went wrong, try again", skin);
+
+        connectButton.addListener(new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                joinGameTable.remove();
+                Network.makeNewClient(ipInput.getText(), Integer.parseInt(portInput.getText()));
+                /*if (Network.getAllClientsOnServer().contains(Network.getClient())) {
+                    connectedTable.add(connectedLabel);
+                }
+                else {
+                    connectedTable.add(connectionFailedLabel);
+                }*/
+                connectedTable.add(connectedLabel);
+                connectedTable.row().padTop(10);
+                connectedTable.add(mainMenu);
+                stage.addActor(connectedTable);
+            }
+        });
 
         mainMenu.addListener(new ChangeListener() {
             @Override
