d9aa0da3ec82afe2b3cfae3eb3d0c8d16236ad38
Mathias Vehus <mathias.vehus@student.no>
2021-03-15 15:05:05 +0100

    makeNewClient() returnerer nå boolean basert på om den klarer å koble klienten opp til serveren. Og slettet en testfil.

--- src/main/java/network/Network.java
+++ src/main/java/network/Network.java
@@ -9,27 +9,33 @@ public class Network {
     private static Server server;
     private static Client client;
 
-    public static void startServer(int port){
-        server =  new Server(port);
+    public static void startServer(int port) {
+        server = new Server(port);
     }
 
-    public static void makeNewClient(String IpAddress, int port){
-        client = new Client(IpAddress, port);
+    public static boolean makeNewClient(String IpAddress, int port) {
+        try {
+            client = new Client(IpAddress, port);
+            return true;
+        } catch (Exception e) {
+            System.out.println("Could not connnect new client to server with exception: \n" + e.toString());
+            return false;
+        }
     }
 
     public static Client getClient() {
         return client;
     }
 
-    public static void disconnectClient(){
+    public static void disconnectClient() {
         client = null;
     }
 
-    public static Server getServer(){
+    public static Server getServer() {
         return server;
     }
 
-    public static ArrayList<Connection> getAllClientsOnServer(){
+    public static ArrayList<Connection> getAllClientsOnServer() {
         return server != null ? server.getClients() : null;
     }
 }
