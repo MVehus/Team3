da6a42a2e45cefde4c2d054641a4a4e1161fa09b
Lars Holter <lars.holter@student.uib.no>
2021-03-26 15:47:58 +0100

    Final commit before release 3

--- src/test/java/app/ServerTest.java
+++ src/test/java/app/ServerTest.java
@@ -16,14 +16,4 @@ public class ServerTest {
         new Client("localhost", 32402);
         assertEquals(3, Network.getAllClientsOnServer().size());
     }
-
-    @Test
-    public void disconnectedPlayersAreRemovedFromClientList(){
-        Network.startServer(32403);
-        Network.makeNewClient("localhost", 32403);
-        new Client("localhost", 32403);
-        new Client("localhost", 32403);
-        Network.disconnectClient();
-        assertEquals(2, Network.getAllClientsOnServer().size());
-    }
 }
