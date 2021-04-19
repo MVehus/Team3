c1c4de3a3a7bca34840daaffe449deea0f134430
Mathias Vehus <mathias.vehus@student.no>
2021-04-19 21:08:14 +0200

    Fixed a test in ServerTest.java

--- src/test/java/app/ServerTest.java
+++ src/test/java/app/ServerTest.java
@@ -11,9 +11,13 @@ public class ServerTest {
     @Test
     public void playerCanConnectToServer() {
         Network.startServer(32402);
-        new Client("localhost", 32402);
-        new Client("localhost", 32402);
-        new Client("localhost", 32402);
+        try {
+            new Client("localhost", 32402);
+            new Client("localhost", 32402);
+            new Client("localhost", 32402);
+        } catch (Exception e) {
+            System.out.println("Test failed with exception: " + e.toString());
+        }
         assertEquals(3, Network.getAllClientsOnServer().size());
     }
 }
